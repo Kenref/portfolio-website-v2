1ea8c539d599984f40d432157bc39da892b208d7
Kenref <107287396+Kenref@users.noreply.github.com>
2024-02-28 21:23:01 +1100

    added scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
+import Spline from "@splinetool/react-spline";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [loading, setLoading] = useState(false);
+	const [scrollable, setScrollable] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const onLoad = (spline) => {
+		const last = spline.findObjectByName("Role");
+		console.log(last);
+		// setLoading(false);
+		// console.log("hi");
+	};
+
+	useEffect(() => {
+		console.log(last);
+	}, [spline.findObjectByName("Role").position.z]);
+
+	if (loading) return <p>loading</p>;
+	return (
+		<div>
+			<Spline
+				scene={"https://prod.spline.design/mxwDdYsyy2Wn90mg/scene.splinecode"}
+				onLoad={onLoad}
+				onError={(error) => console.log(error)}
+				style={{ height: "100vh" }}
+			/>
+		</div>
+	);
 }
 
-export default App
+export default App;
