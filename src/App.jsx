0562f252b9f19773d9534220cee2b0045f4bc4a8
Kenref <107287396+Kenref@users.noreply.github.com>
2024-03-01 19:29:56 +1100

    created skill section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import React, { useEffect, useState, useRef } from "react";
-
 import "./App.css";
 import Spline from "@splinetool/react-spline";
+import SkillSubsection from "./components/SkillSubsection";
 
 // next step figure out how to put skills in a sleek way
 function App() {
+	const languages = { title: "Languages" };
+	const frameLibs = { title: "Libraries / Frameworks" };
+	const other = { title: "Other" };
+
 	return (
 		<div>
 			<Spline
 				scene="https://prod.spline.design/DQiuGtWGXvHI6syP/scene.splinecode"
 				style={{ height: "100vh" }}
 			/>
-			<div style={{ height: "100vh" }}>
-				<div style={{ color: "white" }}>skills</div>
-				<div style={{ color: "white" }}>projects</div>
-				<div style={{ color: "white" }}>contact informations</div>
+			<div
+				style={{
+					height: "100vh",
+				}}
+			>
+				<h1 style={{ textAlign: "center" }}>Skills</h1>
+				<div style={{ display: "flex", justifyContent: "space-around" }}>
+					<SkillSubsection title={languages.title} />
+					<SkillSubsection title={frameLibs.title} />
+					<SkillSubsection title={other.title} />
+				</div>
 			</div>
 		</div>
 	);
---
+++ src/components/SkillSubsection.jsx
@@ -0,0 +1,9 @@
+function SkillSubsection({ title }) {
+	return (
+		<div>
+			<h2>{title}</h2>
+		</div>
+	);
+}
+
+export default SkillSubsection;
