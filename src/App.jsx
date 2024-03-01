823932297e43606e3915c1bca97382d9994fc476
Kenref <107287396+Kenref@users.noreply.github.com>
2024-03-01 19:01:39 +1100

    created new components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 import Spline from "@splinetool/react-spline";
 
+// next step figure out how to put skills in a sleek way
 function App() {
 	return (
 		<div>
@@ -10,10 +11,10 @@ function App() {
 				scene="https://prod.spline.design/DQiuGtWGXvHI6syP/scene.splinecode"
 				style={{ height: "100vh" }}
 			/>
-			<div style={{ height: "100vh", color: "white" }}>skills</div>
-			<div style={{ height: "100vh", color: "white" }}>projects</div>
-			<div style={{ height: "100vh", color: "white" }}>
-				contact informations
+			<div style={{ height: "100vh" }}>
+				<div style={{ color: "white" }}>skills</div>
+				<div style={{ color: "white" }}>projects</div>
+				<div style={{ color: "white" }}>contact informations</div>
 			</div>
 		</div>
 	);
