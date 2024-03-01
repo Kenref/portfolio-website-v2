3210100f4d1c0c608c33d75354b7cdf6f04e6909
Kenref <107287396+Kenref@users.noreply.github.com>
2024-03-02 00:50:30 +1100

    added some styles to skills section

--- src/components/SkillSubsection.jsx
+++ src/components/SkillSubsection.jsx
@@ -5,23 +5,24 @@ import { PropTypes } from "prop-types";
 function SkillSubsection({ title, contents }) {
 	return (
 		<div
+			className="skills-section"
 			style={{
 				display: "flex",
 				flexDirection: "column",
 				alignItems: "center",
+				backgroundColor: "rgba(255,255,255,0.02)",
+				border: "1px solid rgba(255,255,255,0.1)",
+				borderRadius: "10px",
+				position: "relative",
+				// padding: "10px",
 			}}
 		>
 			<h2 style={{ margin: "10px" }}>{title}</h2>
 			<div
-				className="skills-effect"
 				style={{
 					display: "grid",
 					gap: "10px",
 					gridTemplateColumns: "repeat(2, 1fr)",
-					backgroundColor: "rgba(255,255,255,0.02)",
-					border: "1px solid rgba(255,255,255,0.1)",
-					borderRadius: "10px",
-					position: "relative",
 				}}
 			>
 				{contents.map((content, index) => (
